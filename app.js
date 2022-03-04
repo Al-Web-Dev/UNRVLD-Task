90f3ff441b1c8d43d726a4117c32f2026ff25528
Alex Jefferis <alexjefferis@alexs-mbp.home>
2022-03-04 16:59:33 +0000

    accordion animation added

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-// Animate the accordian
-var accordianButton = document.getElementsByClassName("accordian");
+var acc = document.getElementsByClassName("accordian");
 var i;
 
-for (i=0; 1 < accordianButton.length; i++) {
-  accordianButton[i].addEventListener('click', function() {
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+ 
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+    } 
   });
 }
 
 
+//TODO: animate bottle rotation on click
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function(event) {
+    var bottleIcon = acc[i].img;
+    bottleIcon.classList.toggle("bottle__rotate");
+
+  });
+}
