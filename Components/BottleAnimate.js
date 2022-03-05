57a242055a5bfb759e05ce37d75e14c8ee7af123
Alex Jefferis <alexjefferis@alexs-mbp.home>
2022-03-05 22:11:30 +0000

    push bottle spin code

--- Components/BottleAnimate.js
+++ Components/BottleAnimate.js
@@ -1,12 +1,11 @@
 export default function bottleAnimate() {
-	return console.log("get that bottle spinning!!");
+	// return console.log("get that bottle spinning!!");
 
-	//TODO: animate bottle rotation on click
-	// for (i = 0; i < acc.length; i++) {
-	//   acc[i].addEventListener("click", function(event) {
-	//     var bottleIcon = acc[i].img;
-	//     bottleIcon.classList.toggle("bottle__rotate");
+	var accBottle = document.getElementsByClassName("accordian");
+	var bottle = document.getElementsByClassName("accordian");
 
-	//   });
-	// }
+	accBottle.addEventListener("click", (event) => {
+		console.log("clickedd!!!");
+		bottle.classList.toggle("bottle__rotate");
+	});
 }
